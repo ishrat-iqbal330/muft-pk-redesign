c03cb1f2f49402f38d171b5b00eb945dcab619f5
Ishrat Iqbal <contact2sketchesscout@gmail.com>
2024-09-26 13:58:12 +0500

    project init

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home";
+import Contact from "./Pages/Contact";
+import Courses from "./Pages/Courses";
+import Achievements from "./Pages/Achievements";
+
+const App = () => {
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/courses",
+        element: <Courses />,
+      },
+      {
+        path: "/achievements",
+        element: <Achievements />,
+      },
+    ],
+    {
+      basename: "/",
+    }
+  );
+  return <RouterProvider router={router} />;
+};
+
+export default App;
