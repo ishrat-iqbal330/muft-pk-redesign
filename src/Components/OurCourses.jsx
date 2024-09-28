94a940122c3241ad8e2a28a5fa4e0f52cc455c83
Ishrat Iqbal <contact2sketchesscout@gmail.com>
2024-09-28 17:07:29 +0500

    feature/MPR : courses section

---
+++ src/Components/OurCourses.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const OurCourses = () => {
+  return (
+    <div>
+      <div className="flex flex-col justify-center items-center text-center py-28 px-4 w-full bg-gray-100">
+        <h5 className="text-2xl sm:text-3xl lg:text-4xl font-bold max-w-[90vw] sm:max-w-[70vw] md:max-w-[60vw] text-[#23282E]">
+          Expert-Led Courses to Boost Your Skills
+        </h5>
+        <p className="text-base sm:text-lg max-w-[95vw] sm:max-w-[80vw] md:max-w-[65vw] lg:max-w-[60vw] text-gray-500 mt-3">
+          Our courses are designed to prepare you for success in your professional and personal life, helping you stay ahead with practical skills and real-world insights.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default OurCourses;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Hero from "../Components/Hero";
 import Brands from "../Components/Brands";
+import OurCourses from "../Components/OurCourses";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Brands />
+      <OurCourses />
     </div>
   );
 };
