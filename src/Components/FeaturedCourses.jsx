5de2e16dc967294ed43d93ab124204d1791ce30f
Ishrat Iqbal <contact2sketchesscout@gmail.com>
2024-10-02 19:18:52 +0500

    feature/MPR: Featured Courses Changes

--- src/Components/FeaturedCourses.jsx
+++ src/Components/FeaturedCourses.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 const FeaturedCourses = () => {
   return (
-    <div className="w-full mx-auto bg-white py-10 px-4 md:px-10">
-      <div className="flex flex-col items-center bg-gradient-to-r from-[#1a0d29] via-[#0d1033] to-[#1a0d29] text-white px-6 py-8 mt-8 rounded-xl w-full max-w-[90vw] h-[500px]">
+    <div className="w-full mx-auto bg-white py-10 px-4 md:px-12 ">
+      <div className="flex flex-col items-center bg-gradient-to-r from-[#1a0d29] via-[#0d1033] to-[#1a0d29] text-white px-6 py-8 mt-8 rounded-xl w-full max-w-[90vw] h-[500px] mx-auto">
         <h2 className="text-2xl font-semibold text-center w-full min-w-[300px] md:text-3xl lg:text-4xl mt-10">
           Explore Our Highlighted Courses
         </h2>
         <p className="text-center text-gray-300 mt-4 text-sm md:text-base lg:w-[48vw]">
-          Explore our top courses to learn new skills and gain knowledge! Start your journey today and unlock your potential! Whether you want to improve your job skills.
+          Check out our top courses to learn new skills and gain knowledge!
+          Start your journey today and unlock your potential! Whether you want
+          to improve your job skills.
         </p>
       </div>
-      <div className="cards grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-[80vw] w-full mx-auto relative bottom-56 right-6">
+      <div className="cards grid place-content-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 max-w-[80vw] w-full mx-auto relative bottom-56 left-0 right-0 ">
         <div className="card px-4 py-6 bg-white shadow-lg rounded-xl">
           <img
             src="/googleadds.jpg"
@@ -27,7 +29,7 @@ const FeaturedCourses = () => {
             <p>Google Ads Pro course for beginners</p>
           </div>
         </div>
-        <div className="card px-4 py-6 bg-white shadow-lg rounded-xl">
+        <div className="hidden  px-4 py-6 bg-white shadow-lg rounded-xl  lg:block">
           <img
             src="/pythonpro.jpg"
             alt="Python Free Course"
@@ -42,7 +44,7 @@ const FeaturedCourses = () => {
             <p>Python advanced programming course</p>
           </div>
         </div>
-        <div className="card px-4 py-6 bg-white shadow-lg rounded-xl">
+        <div className="card hidden  px-4 py-6 bg-white shadow-lg rounded-xl sm:block lg:block">
           <img
             src="/cyber-security.jpg"
             alt="Cyber Security Course"
