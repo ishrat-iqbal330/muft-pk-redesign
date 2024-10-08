be2d840f45d00172e37d925aee6132a669f57f4b
Ishrat Iqbal <contact2sketchesscout@gmail.com>
2024-10-08 20:07:07 +0500

    feature/MPR: changes in footer compo

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className=' bg-gradient-to-r from-[#1a0d29] via-[#0d1033] to-[#1a0d29]'>
-      <div>
+      <div className='flex flex-col justify-center items-center text-white py-10'>
         
       </div>
     </div>
